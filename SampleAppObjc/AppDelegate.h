cb3e95aaae2f8a48be86c2b699abf93b167646c6
jacky_chen <jacky_chen@cathaylife.com.tw>
2016-12-29 17:04:43 +0800

    Initial Commit

---
+++ SampleAppObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleAppObjc
+//
+//  Created by JackyChen on 2016/12/29.
+//  Copyright © 2016年 JackyChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleAppObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleAppObjc
+//
+//  Created by JackyChen on 2016/12/29.
+//  Copyright © 2016年 JackyChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
